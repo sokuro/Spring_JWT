41c799dee7b540b23920ccf008e5c7214eb3624f
Ugorcak, Karol Sokuro <karol.ugorcak@adesso.ch>
2021-06-29 09:14:50 +0200

    init commit

---
+++ auth0/src/main/java/com/spring/jwt/auth0/Auth0Application.java
@@ -0,0 +1,13 @@
+package com.spring.jwt.auth0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Auth0Application.class, args);
+    }
+
+}
---
+++ auth0/src/test/java/com/spring/jwt/auth0/Auth0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jwt.auth0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
